3c76a2fba1def6f756261cefb255c75dd2b4124e
David Young-Chan Kay <davidykay@gmail.com>
2020-05-08 18:29:30 -0700

    Client: now explicitly connects to 127.0.0.1

--- client.d
+++ client.d
@@ -4,7 +4,7 @@ import std.socket;
 void main() {
   Socket client = new TcpSocket();
   client.setOption(SocketOptionLevel.SOCKET, SocketOption.REUSEADDR, true);
-  client.connect(new InternetAddress(9090));
+  client.connect(new InternetAddress("127.0.0.1", 9090));
   scope(exit) client.close();
 
   auto buffer = new ubyte[2056];
